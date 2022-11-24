80e8f53217d9e329c162c76fb19a362c7b4e4717
HonzaDandy <82872135+HonzaDandy@users.noreply.github.com>
2022-11-24 17:30:18 +0100

    Add files via upload

---
+++ eepromValues.h
@@ -0,0 +1,28 @@
+// eepromValues.h
+
+#ifndef _EEPROMVALUES_h
+#define _EEPROMVALUES_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+typedef  struct S_EEPROM
+{
+
+	unsigned int uiIterator[30];
+	byte bTypFridge;
+
+} sEEValue;
+
+extern sEEValue m_sEEValues;
+
+void EERead();
+void EEWrite();
+
+void EERead(void* pVoid, unsigned int uiSize);
+void EEWrite(void* pVoid, unsigned int uiSize);
+
+#endif
